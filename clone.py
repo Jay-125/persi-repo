43aad92b32a47bda89884edc02a66490d4f6e945
jatin__008 <pathak2000jay@gmail.com>
2021-06-03 15:18:16 +0530

    sub function added

--- clone.py
+++ clone.py
@@ -3,7 +3,10 @@ def add(x,y):
     pass
 #implementation
 def subtract(x,y):
-    return x-y
+    if(x > y):
+        return x-y
+    else:
+	return y-x
 #implementation
 def multiply(x,y):
     pass
